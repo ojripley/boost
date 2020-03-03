77827d3c5630ade941c3d522bf5ce3c9cc1f27fd
ojripley <ojripley19@gmail.com>
2020-03-03 12:15:21 -0500

    add particle effects

--- Assets/Scenes/Rocket.cs
+++ Assets/Scenes/Rocket.cs
@@ -10,6 +10,14 @@ public class Rocket : MonoBehaviour {
 	[SerializeField] float progradeThrustMulitplier = 2000f;
 	[SerializeField] float rotationMultiplier = 400f;
 
+	[SerializeField] AudioClip thrustAudio;
+	[SerializeField] AudioClip deathAudio;
+	[SerializeField] AudioClip transcendAudio;
+
+	[SerializeField] ParticleSystem thrustParticles;
+	[SerializeField] ParticleSystem explosionParticles;
+	[SerializeField] ParticleSystem successParticles;
+
 	int currentLevel = 0;
 
 	enum State { Alive, Dying, Transcending };
@@ -23,9 +31,7 @@ public class Rocket : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update() {
-		handleInput();
-
-		handleAudioOnState();
+		HandleInput();
 	}
 
 	private void LoadFirstLevel() {
@@ -41,71 +47,91 @@ public class Rocket : MonoBehaviour {
 	}
 
 	private void OnCollisionEnter(Collision collision) {
-		switch(collision.gameObject.tag) {
-			case "Friendly":
-				print("on the launch pad");
-				break;
-			case "Finish":
-				print("You beat the level!");
-				state = State.Transcending;
-				Invoke("LoadNextLevel", 1f); // todo: paramatarize time
-				
-				break;
-			case "Fuel":
-				print("Gassed up homie");
-				break;
-			default:
-				print("dead");
-				state = State.Dying;
-				Invoke("LoadFirstLevel", 1f);
-				break;
+		if (state == State.Alive) {
+			switch(collision.gameObject.tag) {
+				case "Friendly":
+					print("on the launch pad");
+					break;
+				case "Finish":
+					HandleLevelComplete();
+					break;
+				case "Fuel":
+					print("Gassed up homie");
+					break;
+				default:
+					HandleDeath();
+					break;
+			}
 		}
 	}
 
-	private void handleInput() {
-		rigidBody.freezeRotation = true; // disallows external control, prevents physics induced spin
+	private void HandleLevelComplete() {
+		audioSource.Stop();
+		audioSource.PlayOneShot(transcendAudio);
 
-		if (state != State.Dying && state != State.Transcending) { // disallow controls while transitioning
+		thrustParticles.Stop();
+		successParticles.Play();
+
+		state = State.Transcending;
+
+		Invoke("LoadNextLevel", 2.1f); // todo: paramatarize time
+	}
+
+	private void HandleDeath() {
+		audioSource.Stop();
+		audioSource.PlayOneShot(deathAudio);
+
+		thrustParticles.Stop();
+		explosionParticles.Play();
+
+		state = State.Dying;
+
+		Invoke("LoadFirstLevel", 2f);
+	}
+
+	private void HandleInput() {
+
+		if (state == State.Alive) { // disallow controls while transitioning
+			rigidBody.freezeRotation = true; // disallows external control, prevents physics induced spin
 
 			// can always thrust, even while rotating
-			if (Input.GetKey(KeyCode.Space)) {
+			RespondToThrustInput();
 
+			//// stop playing when space is released
+			//if (Input.GetKeyUp(KeyCode.Space)) {
+			//	audioSource.Stop();
+			//}
 
-				rigidBody.AddRelativeForce(Vector3.up * progradeThrustMulitplier);
-				handleAudio();
-			}
-
-			// stop playing when space is released
-			if (Input.GetKeyUp(KeyCode.Space)) {
-				audioSource.Stop();
-			}
-
-			if (Input.GetKey(KeyCode.A)) {
-				transform.Rotate(Vector3.forward * rotationMultiplier * Time.deltaTime); // left handed coordinate system in the z-axis. this means +ve values go counter-clockwise
-			} else if (Input.GetKey(KeyCode.D)) {
-				transform.Rotate(Vector3.back * rotationMultiplier * Time.deltaTime);
-			}
+			RespondToRotateInput();
 		}
 
 		rigidBody.freezeRotation = false; // once player control is released, reallow physics induced spin
 	}
 
+	private void RespondToThrustInput() {
+		if (Input.GetKey(KeyCode.Space)) {
 
-	// handles audio on thrust
-	private void handleAudio() {
-		if (!audioSource.isPlaying) { // prevents starting the clip on every frame;
-			audioSource.Play();
+			// make rocket noises bbrrgrgrrrrhhrhrhr
+			if (!audioSource.isPlaying) { // prevents starting the clip on every frame;
+				audioSource.PlayOneShot(thrustAudio);
+				thrustParticles.Play();
+			}
+			
+			// apply thrust
+			rigidBody.AddRelativeForce(Vector3.up * progradeThrustMulitplier);
 		}
 
-		// stop playing when space is released
 		if (Input.GetKeyUp(KeyCode.Space)) {
 			audioSource.Stop();
+			thrustParticles.Stop();
 		}
 	}
 
-	private void handleAudioOnState() {
-		if ((state == State.Dying || state == State.Transcending) && audioSource.isPlaying) {
-			audioSource.Stop();
+	private void RespondToRotateInput() {
+		if (Input.GetKey(KeyCode.A)) {
+			transform.Rotate(Vector3.forward * rotationMultiplier * Time.deltaTime); // left handed coordinate system in the z-axis. this means +ve values go counter-clockwise
+		} else if (Input.GetKey(KeyCode.D)) {
+			transform.Rotate(Vector3.back * rotationMultiplier * Time.deltaTime);
 		}
 	}
 }
